a06fb06f932e6e38d24feaf02db7a7f8773422b1
moaojarco <jomarcofs@gmail.com>
2021-01-08 13:51:12 -0400

    Manipulando o DOM sozinho

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,5 +1,5 @@
 // Remember, we're gonna use strict mode in all scripts now!
-"use strict";
+'use strict';
 
 ///////////////////////////////////////
 // Using Google, StackOverflow and MDN
@@ -7,7 +7,7 @@
 // PROBLEM 1:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // console.log(temperatures);
 // 1) Understanding the problem
 // - What is temp amplitude? Answer: difference between highest and lowest temp
@@ -47,7 +47,7 @@ const calcTempAmplitudeNew = function (t1, t2) {
   let min = temps[0];
   for (let i = 0; i < temps.length; i++) {
     const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
+    if (typeof curTemp !== 'number') continue;
 
     if (curTemp > max) max = curTemp;
     if (curTemp < min) min = curTemp;
--- 04-DOM/script.js
+++ 04-DOM/script.js
@@ -1,1 +1,35 @@
-"use strict";
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🔥 Correct Number';
+
+// document.querySelector('.number').textContent = 13;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.floor(1 + Math.random() * 20);
+console.log(secretNumber);
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  // console.log(guess, typeof guess);
+  if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Um pouco mais...';
+  } else {
+    document.querySelector('.message').textContent = 'Um pouco menos...';
+  }
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'Sem gracinhas, eu disse pra adivinhar um número.';
+    const img = document.querySelector('.img');
+    img.src = './mightguy-not.jpg';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      'Co-como assim? Você acertou o meu número secreto!!!.';
+    const img = document.querySelector('.img');
+    img.src = './mightguy-done.jpg';
+
+    document.querySelector('.score').textContent = guess;
+  }
+});
