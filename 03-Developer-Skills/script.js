5b47cdae6eb356aba8eb4b8a66be68d4f1a53ea8
moaojarco <jomarcofs@gmail.com>
2021-01-08 08:45:40 -0400

    learn debug with chrome debugger

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -8,8 +8,7 @@
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
-console.log(temperatures);
-
+// console.log(temperatures);
 // 1) Understanding the problem
 // - What is temp amplitude? Answer: difference between highest and lowest temp
 // - How to compute max and min temperatures?
@@ -20,3 +19,52 @@ console.log(temperatures);
 // - Find max value in temp array
 // - Find min value in temp array
 // - Subtract min from max (amplitude) and return it
+
+// let max = temperatures[0];
+
+// for (let i = 1; i < temperatures.length; ++i) {
+//   if (temperatures[i] > max) {
+//     max = temperatures[i];
+//   }
+// }
+
+// let min = temperatures[0];
+
+// for (let i = -5; i > temperatures.length; --i) {
+//   if (temperatures[i] < min) {
+//     min = temperatures[i];
+//   }
+// }
+
+// console.log(max, min);
+
+//Function Declaration com loop para pegar o valor máximo e o mínimo do Array.
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+//Armazenando o valor gerado da função em um variável chamada "amplitude"
+const amplitudeNew = calcTempAmplitudeNew([2, 4, 5], [4, 2, 3]);
+console.log(amplitudeNew);
+
+//problema 2
+// A função agora deve receber 2 arrays de temperatura
+
+//1) Entender o problema
+// - Com 2 arrays, nós deveremos implementar a função duas vezes? NÃO! Apenas junte as duas arrays.
+
+//2) Quebrando em pequenos pedaços
+// -  Juntar duas arrays?
