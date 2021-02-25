9749754094e4802c7eb51d20800d5f8db52dc4ef
moaojarco <jomarcofs@gmail.com>
2021-02-25 16:37:26 -0400

    fix all

--- 18-Forkify/src/js/config.js
+++ 18-Forkify/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '600b2395-a3a4-4472-8d97-5e312ce22e06';
 export const MODAL_CLOSE_SEC = 2.5;
