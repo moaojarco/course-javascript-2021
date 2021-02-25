a972105e7745e0e9c52bd9d7dcc41a24059a6ca6
moaojarco <jomarcofs@gmail.com>
2021-02-25 16:25:22 -0400

    done app

--- 18-Forkify/src/js/views/searchView.js
+++ 18-Forkify/src/js/views/searchView.js
@@ -1,23 +1,22 @@
 class SearchView {
-    #parentEl = document.querySelector('.search');
+  _parentEl = document.querySelector('.search');
 
-    getQuery() {
-        const query = this.#parentEl.querySelector('.search__field').value;
-        this.#clearInput();
-        return query;
-    }
-    
-    #clearInput() {
-        this.#parentEl.querySelector('.search__field').value = '';
-    }
+  getQuery() {
+    const query = this._parentEl.querySelector('.search__field').value;
+    this._clearInput();
+    return query;
+  }
 
+  _clearInput() {
+    this._parentEl.querySelector('.search__field').value = '';
+  }
 
-    addHandlerSearch(handler) {
-        this.#parentEl.addEventListener('submit', function (e) {
-            e.preventDefault();
-            handler();
-        });
-    }
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
+    });
+  }
 }
 
 export default new SearchView();
